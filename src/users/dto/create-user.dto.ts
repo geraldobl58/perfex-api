03389ebee8a5c517f299a247840599955353c7e3
Geraldo Luiz <geraldobl58@gmail.com>
2024-10-15 17:06:00 -0300

    feat: Creating data validation when creating and editing users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,23 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsInt()
   phone: string;
+
+  @IsBoolean()
   isAdmin: boolean;
 }
