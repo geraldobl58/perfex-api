a6ae35e7d6c3f17fcda362116059d421eb64d981
Geraldo Luiz <geraldobl58@gmail.com>
2024-10-16 12:35:02 -0300

    feat: Creating exception filters

---
+++ src/common/filters/http-exception/http-exception.filter.ts
@@ -0,0 +1,32 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter<T extends HttpException>
+  implements ExceptionFilter
+{
+  catch(exception: T, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const res = ctx.getResponse<Response>();
+
+    const status = exception.getStatus();
+    const exceptionRes = exception.getResponse();
+
+    const error =
+      typeof res === 'string'
+        ? {
+            message: exceptionRes,
+          }
+        : (exceptionRes as Record<string, unknown>);
+
+    res.status(status).json({
+      ...error,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
