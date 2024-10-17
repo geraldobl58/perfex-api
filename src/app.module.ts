6668a0611f00532639f568376285f6d3fcd4e573
Geraldo Luiz <geraldobl58@gmail.com>
2024-10-17 16:35:01 -0300

    chore: Removing main controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, ProductsModule, CategoriesModule],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    ProductsModule,
+    CategoriesModule,
+  ],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
